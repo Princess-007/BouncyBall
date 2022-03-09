4da07d5af63e2018b5fcd0b5421afa6f2f63a43d
Paula Coombs <coombsy0007@gmail.com>
2022-03-08 22:42:06 -0500

    Paula Coombs-Johnson March 8, 2022

--- GameCode.swift
+++ GameCode.swift
@@ -1,5 +1,5 @@
 import Foundation
-
+let ball = OvalShape(width: 40, height: 40)
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
 Use it to set up and start your app.
@@ -10,6 +10,106 @@ for example if statements and for loops, at the top level; they have to be writt
 of a function.
 */
 
-func setup() {
-    
+// Code to setup the Ball
+
+fileprivate func setupBall() {
+    ball.position = Point(x: 250, y: 400)
+    scene.add(ball)
+    ball.hasPhysics = true
+    ball.fillColor = .orange
 }
+
+fileprivate func setupBarrier() {
+    // Add a barrier to the scene.
+    barrier.position = Point(x: 200, y: 150)
+    barrier.hasPhysics = true
+    scene.add(barrier)
+    barrier.isImmobile = true
+}
+
+fileprivate func setupFunnel() {
+    // Add a funnel to the scene.
+    funnel.position = Point(x: 200, y:
+    scene.height - 25)
+    scene.add(funnel)
+    
+    funnel.onTapped = dropBall
+    funnel.fillColor = .purple
+}
+
+func setup() {
+    setupBall()
+    ball.onCollision = ballCollided(with:)
+    
+    setupBarrier()
+    
+    
+    setupFunnel()
+   
+// Add a target to the scene - page 238.
+    
+    setupTarget()
+    target.name = "target"
+}
+
+func dropBall() {
+    ball.position = funnel.position
+}
+
+
+let barrierWidth = 300.0
+let barrierHeight = 25.0
+
+let barrierPoints = [
+    Point(x: 0, y: 0),
+    Point(x: 0, y: barrierHeight),
+    Point(x: barrierWidth, y:
+       barrierHeight),
+    Point(x: barrierWidth, y: 0)
+]
+
+let barrier = PolygonShape(points:
+   barrierPoints)
+
+let funnelPoints = [
+    Point(x: 0, y: 50),
+    Point(x: 80, y: 50),
+    Point(x: 60, y: 0),
+    Point(x: 20, y: 0)
+]
+
+let funnel = PolygonShape(points:
+    funnelPoints)
+
+// Add a Target Shape
+
+let targetPoints = [
+    Point(x: 10, y: 0),
+    Point(x: 0, y: 10),
+    Point(x: 10, y: 20),
+    Point(x: 20, y: 10)
+]
+
+let target = PolygonShape(points:
+    targetPoints)
+
+// A function to for Target Shape
+
+func setupTarget() {
+    target.position = Point(x: 200, y: 400)
+    target.hasPhysics = true
+    target.isImmobile = true
+    target.isImpermeable = false
+    target.fillColor = .yellow
+
+    scene.add(target)
+}
+
+// Handles collisions between the ball and the targets - page 240.
+
+func ballCollided(with otherShape: Shape) {
+    if otherShape.name != "target" { return }
+    
+    otherShape.fillColor = .green
+}
+
